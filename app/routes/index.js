68afcf1660261871649f16313c8a2581c53edc71
Theodoros Itzaris <theodoreme@gmail.com>
2018-08-01 10:18:08 +0300

    Make '/fruit-machine', main page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  // make default page `/fruit-machine`
+  beforeModel() {
+    this.replaceWith('fruit-machine')
+  }
 });
